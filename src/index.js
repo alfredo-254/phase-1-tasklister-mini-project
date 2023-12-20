7a988b742e8b35ec3ae4f1091a99d15c3b6cf2de
alfred kivuva <kivuvakioko97@gmail.com>
2023-12-21 00:01:16 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const checkbox = document.querySelector("#id-checkbox");
+
+checkbox.addEventListener("click", checkboxClick, false);
+
+function checkboxClick(event) {
+  let warn = "preventDefault() won't let you check this!<br>";
+  document.getElementById("output-box").innerHTML += warn;
+  event.preventDefault();
+}
 });
+
+function deleteTask(button) {
+  // Get the parent <li> element of the clicked button
+  const taskItem = button.parentElement;
+
+  // Remove the task item from the task list
+  taskList.removeChild(taskItem);
+}
